bd3ec0bd0c677cb1da8507cfc07fdce407ca6f27
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-02-04 17:07:58 +0530

    Update ChildComponent.js

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const ChildComponent = () => {
-//code here
+
+const ChildComponent = ({input}) => {
+  
   return (
     <div id="child">
       Find The Square <br/>
-     
+     {input}
     </div>
   )
 }
