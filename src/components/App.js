405d45de356af720d7725e73fa5c22e9b13885e0
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-02-04 17:06:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,18 @@ import React , {useState,useEffect} from 'react'
 import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
- //code here
+  const[input1, setInput1] = useState(0)
 
   return (
     <div id="main">
-      <ChildComponent /> 
-       <input id='input' onChange={} />
-    
-      <p id='output'>{}</p>
+      <ChildComponent  input ={ 
+        <input id='input' onChange={(e) => {
+            let ans = e.target.value;
+            ans = Math.pow(ans, 2);
+            setInput1(ans);
+        }} />
+      }/>
+      <p id='output'>{input1}</p>
     </div>
   )
 }
